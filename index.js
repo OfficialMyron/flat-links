5e7ccaa9f963f6762a58d9514e7f205f523b9d5e
CodeTyper <106383750+RobloxCodeTyper@users.noreply.github.com>
2022-11-05 15:44:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 
-app.get('*', function(req, res){
-  res.redirect('http://tumblr.com/dashboard');
+app.get('/portal', function(req, res){
+  res.redirect('https://roblox.com');
 });
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
