465461c1e376ca0289126ed0c83ccebc254606d0
CodeTyper <106383750+RobloxCodeTyper@users.noreply.github.com>
2022-11-05 09:17:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express = require('express');
 const bodyParser = require('body-parser');
+var express = require('express');
 var app = express();
 app.get('/portal', function(req, res) {
     res.redirect(`https://discord.com`);
